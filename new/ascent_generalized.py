f27ae39471c309e027c7bb6b0c5ebab7ab8658a3
Filippo <filippo.corradino@gmail.com>
2025-12-29 00:57:21 +0100

    Backwards-compatible improvements
    - Better stream handling
    - Prepare for generalized logging
    - Continuous engine parameters filter
    - Separate and develop terminal guidance
    - Detect and display max Q

--- new/ascent_generalized.py
+++ new/ascent_generalized.py
@@ -3,7 +3,7 @@ import csv
 import threading
 import time
 import signal
-from math import sin, asin, tan, atan2, cos, degrees, radians
+from math import sqrt, sin, asin, cos, degrees, radians
 
 from krpc.error import StreamError
 import krpc
@@ -11,18 +11,43 @@ import krpc
 from guidance import peg
 
 
+# Physical constants
 G0 = 9.80665
 R_EARTH = 6371000
 MU_EARTH = 398600000000000
-T_PEG_HARD_CUTOFF = 10
+
+# Guidance settings
+T_PEG_HARD_CUTOFF = 10  # TODO: bring in guidance
+T_GUIDANCE_MARGIN = 5  # How many seconds past tgo to wait for termination before shutdown
+
+# Settings for complementary filter estimating T, Isp
+ENGINE_FILTER_ALPHA = .9
+ENGINE_FILTER_MIN_T = 2
+ENGINE_FILTER_MIN_N = 10
+
+
+def dot(a, b):
+    return sum(x*y for x, y in zip(a, b))
+
+def cross(a, b):
+    return (a[1]*b[2] - a[2]*b[1],
+            a[2]*b[0] - a[0]*b[2],
+            a[0]*b[1] - a[1]*b[0])
 
 
 class LoggingThread(threading.Thread):
 
-    def __init__(self, log_sample_time, tm_dict):
+    def __init__(self, conn, vessel, log_sample_time, tm_dict):
         super().__init__()
         self.log_sample_time = log_sample_time
-        self.tm_dict = tm_dict
+        self.tm_dict = {}
+        rf = vessel.orbit.body.reference_frame
+        for k, v in tm_dict['vessel']:
+            self.tm_dict[k] = conn.add_stream(getattr, vessel, v)
+        for k, v in tm_dict['orbit']:
+            self.tm_dict[k] = conn.add_stream(getattr, vessel.orbit, v)
+        for k, v in tm_dict['flight']:
+            self.tm_dict[k] = conn.add_stream(getattr, vessel.flight(rf), v)
         self.stopped = False
 
     def run(self):
@@ -44,9 +69,8 @@ class LoggingThread(threading.Thread):
 
 class SteeringThread(threading.Thread):
     
-    def __init__(self, conn, vessel, ut_stream, frame):
+    def __init__(self, vessel, ut_stream, frame):
         super().__init__()
-        self.conn = conn
         self.vessel = vessel
         self.ut = ut_stream
         self.vessel.auto_pilot.reference_frame = frame
@@ -55,9 +79,12 @@ class SteeringThread(threading.Thread):
     def run(self):
         while not self.stopped:
             dt = self.ut() - self.t0
+            if dt > self.T + T_GUIDANCE_MARGIN:
+                break
             self.vessel.auto_pilot.target_direction = \
                 tuple(d + dt*dd for d, dd in zip(self.dir, self.ddir))
             time.sleep(0.1)
+        self.stop()
     
     def update(self, t0, dir, ddir, T):
         self.t0 = t0
@@ -84,12 +111,18 @@ class FairingThread(threading.Thread):
         self.stopped = False
 
     def run(self):
+        max_q = False
+        q = 0
         while not self.stopped:
             h = self.h()
-            q = self.q()
+            dq = self.q() - q
+            q += dq
             if (h > self.fairings_minimum_altitude and
                 q < self.fairings_dynamic_pressure):
                 break
+            if not max_q and dq < 0:
+                print("Max Q")
+                max_q = True
             time.sleep(1)
         print(f"Fairing Jett")
         self.vessel.control.set_action_group(self.fairings_action_group, True)
@@ -137,7 +170,6 @@ class StagingThread(threading.Thread):
         # Stage Start events
         self.stage(n=self.stage_s_events)
         if self.ullage:
-            # self.vessel.control.rcs = True
             time.sleep(self.ullage)
         # Stage Ignition events
         self.vessel.control.throttle = 1.0
@@ -148,9 +180,6 @@ class StagingThread(threading.Thread):
             print(f"{self.name} Ignition")
         self.ignited = True
         self.stage(n=self.stage_i_events)
-        if self.ullage:
-            time.sleep(1)
-            self.vessel.control.rcs = False
         # Stage End events
         event = self.conn.krpc.add_event(burnout)
         with event.condition:
@@ -177,45 +206,14 @@ class Mission():
         self.conn = conn
         self.vessel = conn.space_center.active_vessel
         self.ut = self.conn.add_stream(getattr, self.conn.space_center, 'ut')
-        self.altitude = self.conn.add_stream(getattr, self.vessel.flight(), 'mean_altitude')
-        self.pressure = self.conn.add_stream(getattr, self.vessel.flight(), 'static_pressure')
-        self.q = self.conn.add_stream(getattr, self.vessel.flight(), 'dynamic_pressure')
-        self.aspeed = self.conn.add_stream(getattr, self.vessel.flight(self.vessel.orbit.body.reference_frame), 'speed')
-        self.vspeed = self.conn.add_stream(getattr, self.vessel.flight(self.vessel.orbit.body.reference_frame), 'vertical_speed')
-        self.ospeed = self.conn.add_stream(getattr, self.vessel.orbit, 'speed')
-        self.Pe = self.conn.add_stream(getattr, self.vessel.orbit, 'periapsis')
-        self.Ap = self.conn.add_stream(getattr, self.vessel.orbit, 'apoapsis')
-        self.r_vec = self.conn.add_stream(self.vessel.position, self.vessel.orbit.body.non_rotating_reference_frame)
-        self.v_vec = self.conn.add_stream(self.vessel.velocity, self.vessel.orbit.body.non_rotating_reference_frame)
-        self.mach = self.conn.add_stream(getattr, self.vessel.flight(), 'mach')
-        self.CD = self.conn.add_stream(getattr, self.vessel.flight(), 'drag_coefficient')
-        self.mass = self.conn.add_stream(getattr, self.vessel, 'mass')
-        self.thrust = self.conn.add_stream(getattr, self.vessel, 'thrust')
-        self.Isp = self.conn.add_stream(getattr, self.vessel, 'specific_impulse')
-        # self.apoapsis_altitude = self.conn.add_stream(getattr, self.vessel.orbit, 'apoapsis_altitude')
-        # self.periapsis_altitude = self.conn.add_stream(getattr, self.vessel.orbit, 'periapsis_altitude')
-        # self.time_to_apoapsis = self.conn.add_stream(getattr, self.vessel.orbit, 'time_to_apoapsis')
-        # self.pitch = self.conn.add_stream(getattr, self.vessel.flight(), 'pitch')
         self.logging_thread = None
         self.stopped = False
 
-    def start_logging(self, log_data, log_sample_time):
-        if log_data:
-            tm_dict = {'UT': self.ut,
-                       'h': self.altitude,
-                       'p': self.pressure,
-                       'va': self.aspeed,
-                       'vo': self.ospeed,
-                       'M': self.mach,
-                       'm': self.mass,
-                       'T': self.thrust,
-                       'Isp': self.Isp,
-                       'CD': self.CD
-                       }
-            self.logging_thread = LoggingThread(log_sample_time, tm_dict)
-            self.logging_thread.start()
-            print("Logging started")
-
+    def start_logging(self, log_sample_time, tm_dict):
+        self.logging_thread = LoggingThread(self.conn, self.vessel, log_sample_time, tm_dict)
+        self.logging_thread.start()
+        print("Logging started")
+    
     def get_isp(self):
         active_engines = [e for e in self.vessel.parts.engines
                           if e.active and e.has_fuel]
@@ -236,9 +234,10 @@ class Mission():
         exit()
 
     def vertical_ascent(self, end_altitude, heading):
+        altitude_stream = self.conn.add_stream(getattr, self.vessel.flight(), 'mean_altitude')
         while True:
             try:
-                start_altitude = self.altitude()
+                start_altitude = altitude_stream()
                 break
             except StreamError:
                 print("Streamerror")
@@ -274,29 +273,31 @@ class Mission():
         # Linear roll program
         print("Roll program")
         while True:
-            altitude = self.altitude()
-            k = ((altitude-start_altitude) / (end_altitude-start_altitude))**.5 * 1.1
+            k = ((altitude_stream()-start_altitude) / (end_altitude-start_altitude))**.5 * 1.1
             roll_cmd =  heading * k + 90 * (1-k)  # Roll program
             if k >= 1:
                 break
             self.vessel.auto_pilot.target_pitch_and_heading(90, roll_cmd)
+            time.sleep(.02)
         event = self.conn.krpc.add_event(altitude_target_reached)
         with event.condition:
             event.wait()
 
     def pitch_program(self, end_altitude, pitch_target, heading):
+        altitude_stream = self.conn.add_stream(getattr, self.vessel.flight(), 'mean_altitude')
         # Linear pitch program
         print("Pitch program")
         self.vessel.auto_pilot.engage()
         self.vessel.auto_pilot.target_pitch_and_heading(90, heading)
-        start_altitude = self.altitude()
+        start_altitude = altitude_stream()
         while True:
-            altitude = self.altitude()
+            altitude = altitude_stream()
             k = ((altitude-start_altitude) / (end_altitude-start_altitude))**.5
             pitch_cmd =  pitch_target * k + 90 * (1-k)  # Pitch program
             if altitude >= end_altitude:
                 break
             self.vessel.auto_pilot.target_pitch_and_heading(pitch_cmd, heading)
+            time.sleep(.02)
         # Gravity turn
         print("Gravity turn")
         self.vessel.control.sas = True
@@ -309,32 +310,40 @@ class Mission():
     def closed_loop_ascent(self, args):
         # Injection into orbit
         print('Closed loop guidance')
+        # Streams
+        thrust_stream = self.conn.add_stream(getattr, self.vessel, 'thrust')
+        mass_stream = self.conn.add_stream(getattr, self.vessel, 'mass')
+        r_vec_stream = self.conn.add_stream(self.vessel.position, self.vessel.orbit.body.non_rotating_reference_frame)
+        v_vec_stream = self.conn.add_stream(self.vessel.velocity, self.vessel.orbit.body.non_rotating_reference_frame)
+        # TODO: COMPUTE CONSTRAINTS
         nT = radians(args.tgt_closed_loop_true_anomaly)
         eT = args.tgt_closed_loop_eccentricity
         rT = args.tgt_closed_loop_altitude + R_EARTH
         pT = rT * (1 + eT*cos(nT))
         drT = (MU_EARTH / pT)**.5 * eT * sin(nT)
         hT = (MU_EARTH * pT)**.5
-        # Average T, Isp
-        Isp = 0
-        thrust = 0
-        avg_n = 10
-        avg_t = 2
-        for _ in range(avg_n):
-            Isp += self.get_isp() / avg_n
-            thrust += self.thrust() / avg_n
-            time.sleep(avg_t / avg_n)
+        # Engine parameters filter
+        thrust = thrust_stream()
+        Isp = self.get_isp()  # FIXME with streams (why standard stream gets Isp=0?)
         initialized = False
+        n_filter = 0
+        dt_filter = ENGINE_FILTER_MIN_T / ENGINE_FILTER_MIN_N
         print(f"Average Thrust {thrust/1000:5.1f} kN - Average Isp {Isp:.0f} s")
         # Closed Loop
         while True:
+            Isp = Isp*ENGINE_FILTER_ALPHA + (1-ENGINE_FILTER_ALPHA)*self.get_isp()
+            thrust = thrust*ENGINE_FILTER_ALPHA + (1-ENGINE_FILTER_ALPHA)*thrust_stream()
+            if n_filter < ENGINE_FILTER_MIN_N:
+                n_filter += 1
+                time.sleep(dt_filter)
+                continue
             ve = Isp * G0
-            tau = ve * self.mass() / thrust
+            tau = ve * mass_stream() / thrust
             if not initialized:
                 T = 0.995 * tau
             t0 = self.ut()
-            r0_vec = self.r_vec()
-            v0_vec = self.v_vec()
+            r0_vec = r_vec_stream()
+            v0_vec = v_vec_stream()
             A, B, C0, CT, T, converged = peg(r0_vec, v0_vec, hT, rT, drT, ve, tau, MU_EARTH, T)
             if converged:
                 try:
@@ -346,7 +355,7 @@ class Mission():
                 if not initialized:
                     self.vessel.auto_pilot.engage()
                     self.vessel.control.sas = False
-                    self.steering_thread = SteeringThread(self.conn, self.vessel, self.ut, self.vessel.surface_reference_frame)
+                    self.steering_thread = SteeringThread(self.vessel, self.ut, self.vessel.surface_reference_frame)
                 pitch_0 = asin(A+C0)
                 pitch_T = asin(A+CT+B*T)
                 hdg = radians(args.heading)
@@ -369,69 +378,63 @@ class Mission():
             A = A - B * dt
             if (T < args.ref_closed_loop_ttgo and not converged) or T < T_PEG_HARD_CUTOFF:
                 break
+    
+    def terminal_guidance(self):
+        # TODO: active guidance rather than just cutoff?
+        r_vec_stream = self.conn.add_stream(self.vessel.position, self.vessel.orbit.body.non_rotating_reference_frame)
+        v_vec_stream = self.conn.add_stream(self.vessel.velocity, self.vessel.orbit.body.non_rotating_reference_frame)
+        Pe_stream = self.conn.add_stream(getattr, self.vessel.orbit, 'periapsis')
+        Ap_stream = self.conn.add_stream(getattr, self.vessel.orbit, 'apoapsis')
         # Completion
         dt = .15
-        kP = .01  # rad/s
-        PeT = pT / (1+eT) - R_EARTH
-        ApT = pT / (1-eT) - R_EARTH
+        # TODO: COMPUTE CONSTRAINTS
+        nT = radians(args.tgt_closed_loop_true_anomaly)
+        eT = args.tgt_closed_loop_eccentricity
+        rT = args.tgt_closed_loop_altitude + R_EARTH
+        pT = rT * (1 + eT*cos(nT))
+        hT = (MU_EARTH * pT)**.5
+        reached = False
         while True:
-            t1 = self.ut()
-            Pe = self.Pe()
-            Ap = self.Ap()
             time.sleep(dt)
-            t2 = self.ut()
-            if t2-t1 == 0:
-                continue
-            nPe = self.Pe()
-            nAp = self.Ap()
-            dr0 = self.vspeed()
-            r0 = self.altitude() + R_EARTH
-            w0 = (self.ospeed()**2 - dr0**2)**.5 / r0
-            h0 = w0 * r0**2
-            if h0 >= hT:
-                self.steering_thread.update(t0, (0, 0, 0), (0, 0, 0), 0)
-                self.vessel.control.throttle = 0.0
-                print("Target reached")
+            if self.steering_thread.stopped:
                 break
-            dPe = (nPe-Pe) / (t2-t1)
-            dAp = (nAp-Ap) / (t2-t1)
-            ttPe = (PeT - nPe) / dPe
-            ttAp = (ApT - nAp) / dAp
-            P = asin(self.steering_thread.dir[0])
-            if (ttPe-ttAp) * dr0 * (rT - nAp) > 0:
-                P -= kP * (t2-t1)  # Pitch down if Pe lagging and behind or leading and ahead 
-            else:
-                P += kP * (t2-t1)  # Pitch up if Pe lagging and ahead or leading and behind
-            # self.steering_thread.update(t2, sin(P), 0, 0, 10)
+            h0_vec = cross(r_vec_stream(), v_vec_stream())
+            h0 = sqrt(dot(h0_vec, h0_vec))
+            if h0 >= hT:
+                self.steering_thread.update(0, (0, 0, 0), (0, 0, 0), 0)
+                self.vessel.control.throttle = 0.0
+                reached = True
+                break
             print(f" FINALIZING "
-                  f" | ttPe {max(-99.9,min(99.9,ttPe)):5.1f} s "
-                  f" | ttAp {max(-99.9,min(99.9,ttAp)):5.1f} s"
-                  f" | Pe {self.Pe()-R_EARTH:8.0f} km | Ap {self.Ap()-R_EARTH:8.0f} km"
-                  f" | P0: {degrees(P):+05.1f} deg")
+                  f" | Pe {Pe_stream()-R_EARTH:8.0f} m | Ap {Ap_stream()-R_EARTH:8.0f} m")
+        if reached:
+            print("Target reached")
+            print(f"Final orbit: {(Pe_stream()-R_EARTH)/1e3:.0f} km x {(Ap_stream()-R_EARTH)/1e3:.0f} km")
         time.sleep(.1)
         self.steering_thread.stop()
         self.steering_thread.join()
-        # self.vessel.control.throttle = 0.0
         self.vessel.auto_pilot.disengage()
         print("Orbital insertion complete!")
 
     def execute(self, args):
-        self.start_logging(args.log_data, args.log_sample_time)
+        if args.log_data:
+            self.start_logging(args.log_sample_time, {})
         self.steering_thread = None
+        self.fairings_thread = FairingThread(self.conn, self.vessel, args)
         # First stage
         self.stage_thread = StagingThread(self.conn, self.vessel, args, n_stage=1)
         self.stage_thread.start()
         self.vertical_ascent(args.ref_vert_ascent_altitude, args.heading)
+        self.fairings_thread.start()
         self.pitch_program(args.ref_pitch_progr_altitude, args.ref_pitch_progr_end_pitch, args.heading)
         # Second stage
-        self.fairings_thread = FairingThread(self.conn, self.vessel, args)
-        self.fairings_thread.start()
         self.stage_thread = StagingThread(self.conn, self.vessel, args, n_stage=2)
         self.stage_thread.start()
         while not self.stage_thread.ignited:
             time.sleep(1)
         time.sleep(1)
         self.closed_loop_ascent(args)
+        self.terminal_guidance()
 
 
 if __name__ == '__main__':
